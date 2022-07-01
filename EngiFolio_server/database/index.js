2d6bbd316f3b315721ef4a9311ccdc985bdd0bf1
piyus <piyushvyawahare2001@gmail.com>
2022-07-01 22:18:49 +0530

    -

--- EngiFolio_server/database/index.js
+++ EngiFolio_server/database/index.js
@@ -1,5 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports.init = function(){
   const mongoose = require("mongoose");
+  console.log(process.env)
   mongoose.connect("mongodb+srv://ecom:1234567890@cluster0.tt9fs.mongodb.net/EngiFolio?retryWrites=true&w=majority")
   .then(function(){
       console.log("db is live");
